38614d5ef6e349e7e9bfa9002accf249f1af98cd
ismailjosim <ismailjosim99@gmail.com>
2025-05-08 22:02:39 +0600

    uncomment all solution and relove index.js

--- index.ts
+++ index.ts
@@ -1,74 +1,74 @@
 // Problem 01
-// function formatString(input: string, toUpper?: boolean): string {
-// 	if (toUpper || toUpper === undefined) {
-// 		return input.toUpperCase()
-// 	} else {
-// 		return input.toLowerCase()
-// 	}
-// }
+function formatString(input: string, toUpper?: boolean): string {
+	if (toUpper || toUpper === undefined) {
+		return input.toUpperCase()
+	} else {
+		return input.toLowerCase()
+	}
+}
 
 // Problem 02
-// function filterByRating(
-// 	items: { title: string; rating: number }[],
-// ): { title: string; rating: number }[] {
-// 	const matching = items.filter(
-// 		(item: { title: string; rating: number }) => item.rating >= 4,
-// 	)
-// 	return matching
-// }
+function filterByRating(
+	items: { title: string; rating: number }[],
+): { title: string; rating: number }[] {
+	const matching = items.filter(
+		(item: { title: string; rating: number }) => item.rating >= 4,
+	)
+	return matching
+}
 
 // Problem 03
-// function concatenateArrays<T>(...arrays: T[][]): T[] {
-// 	// return arrays.flat()
-// 	return arrays.reduce((acc, cur) => acc.concat(cur), [])
-// }
+function concatenateArrays<T>(...arrays: T[][]): T[] {
+	// return arrays.flat()
+	return arrays.reduce((acc, cur) => acc.concat(cur), [])
+}
 
 // problem 04
-// class Vehicle {
-// 	private make: string
-// 	private year: number
-// 	constructor(make: string, year: number) {
-// 		this.make = make
-// 		this.year = year
-// 	}
-// 	getInfo(): string {
-// 		return `Make: ${this.make}, Year: ${this.year}`
-// 	}
-// }
+class Vehicle {
+	private make: string
+	private year: number
+	constructor(make: string, year: number) {
+		this.make = make
+		this.year = year
+	}
+	getInfo(): string {
+		return `Make: ${this.make}, Year: ${this.year}`
+	}
+}
 
-// class Car extends Vehicle {
-// 	private model: string
-// 	constructor(make: string, year: number, model: string) {
-// 		super(make, year)
-// 		this.model = model
-// 	}
-// 	getModel(): string {
-// 		return `Model: ${this.model}`
-// 	}
-// }
+class Car extends Vehicle {
+	private model: string
+	constructor(make: string, year: number, model: string) {
+		super(make, year)
+		this.model = model
+	}
+	getModel(): string {
+		return `Model: ${this.model}`
+	}
+}
 
 // Problem 05
-// function processValue(value: string | number): number {
-// 	if (typeof value === 'string') {
-// 		return value.length
-// 	} else {
-// 		return value * 2
-// 	}
-// }
+function processValue(value: string | number): number {
+	if (typeof value === 'string') {
+		return value.length
+	} else {
+		return value * 2
+	}
+}
 
 // Problem 06
-// interface Product {
-// 	name: string
-// 	price: number
-// }
+interface Product {
+	name: string
+	price: number
+}
 
-// function getMostExpensiveProduct(products: Product[]): Product | null {
-// 	if (products.length === 0) return null
+function getMostExpensiveProduct(products: Product[]): Product | null {
+	if (products.length === 0) return null
 
-// 	return products.reduce((max, product) =>
-// 		product.price > max.price ? product : max,
-// 	)
-// }
+	return products.reduce((max, product) =>
+		product.price > max.price ? product : max,
+	)
+}
 
 // Problem 07
 enum Day {
